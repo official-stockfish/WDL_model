c284e58973dff075d71625924875792aab5422e6
disservin <disservin.social@gmail.com>
2023-01-11 00:06:28 +0100

    display progress (#3)

--- scoreWLDstat.py
+++ scoreWLDstat.py
@@ -6,84 +6,93 @@ import re
 import json
 import os
 import argparse
+from tqdm import tqdm
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 class PosAnalyser:
     def __init__(self, plies):
         self.matching_plies = plies
 
-    def ana_pos(self, filename):
-        pgnfilein = open(filename, "r", encoding="utf-8-sig", errors="surrogateescape")
-        gameCounter = 0
+    def ana_pos(self, files):
         matstats = Counter()
+        gameCounter = 0
+        for filename in files:
+            pgnfilein = open(filename, "r", encoding="utf-8-sig", errors="surrogateescape")
 
-        p = re.compile("([+-]*M*[0-9.]*)/([0-9]*)")
-        mateRe = re.compile("([+-])M[0-9]*")
 
-        while True:
-            # read game
-            game = chess.pgn.read_game(pgnfilein)
-            if game == None:
-                break
 
-            gameCounter = gameCounter + 1
+            p = re.compile("([+-]*M*[0-9.]*)/([0-9]*)")
+            mateRe = re.compile("([+-])M[0-9]*")
 
-            # get result
-            result = game.headers["Result"]
-            if result == "1/2-1/2":
-                resultkey = "D"
-            elif result == "1-0":
-                resultkey = "W"
-            elif result == "0-1":
-                resultkey = "L"
-            else:
-                continue
-
-            # look at the game,
-            plies = 0
-            board = game.board()
-            for node in game.mainline():
-
-                plies = plies + 1
-                if plies > 400:
+            while True:
+                # read game
+                game = chess.pgn.read_game(pgnfilein)
+                if game == None:
                     break
-                plieskey = (plies + 1) // 2
 
-                turn = board.turn
-                scorekey = None
-                m = p.search(node.comment)
-                if m:
-                    score = m.group(1)
-                    m = mateRe.search(score)
+                gameCounter = gameCounter + 1
+
+                # get result
+                result = game.headers["Result"]
+                if result == "1/2-1/2":
+                    resultkey = "D"
+                elif result == "1-0":
+                    resultkey = "W"
+                elif result == "0-1":
+                    resultkey = "L"
+                else:
+                    continue
+
+                # look at the game,
+                plies = 0
+                board = game.board()
+                for node in game.mainline():
+
+                    plies = plies + 1
+                    if plies > 400:
+                        break
+                    plieskey = (plies + 1) // 2
+
+                    turn = board.turn
+                    scorekey = None
+                    m = p.search(node.comment)
                     if m:
-                        if m.group(1) == "+":
-                            score = 1001
+                        score = m.group(1)
+                        m = mateRe.search(score)
+                        if m:
+                            if m.group(1) == "+":
+                                score = 1001
+                            else:
+                                score = -1001
                         else:
-                            score = -1001
-                    else:
-                        score = int(float(score) * 100)
-                        if score > 1000:
-                            score = 1000
-                        elif score < -1000:
-                            score = -1000
-                        score = (score // 5) * 5  # reduce precision
-                    if turn == chess.BLACK:
-                        score = -score
-                    scorekey = score
+                            score = int(float(score) * 100)
+                            if score > 1000:
+                                score = 1000
+                            elif score < -1000:
+                                score = -1000
+                            score = (score // 5) * 5  # reduce precision
+                        if turn == chess.BLACK:
+                            score = -score
+                        scorekey = score
 
-                knights = bin(board.knights).count("1")
-                bishops = bin(board.bishops).count("1")
-                rooks = bin(board.rooks).count("1")
-                queens = bin(board.queens).count("1")
-                pawns = bin(board.pawns).count("1")
+                    knights = bin(board.knights).count("1")
+                    bishops = bin(board.bishops).count("1")
+                    rooks = bin(board.rooks).count("1")
+                    queens = bin(board.queens).count("1")
+                    pawns = bin(board.pawns).count("1")
 
-                matcountkey = 9 * queens + 5 * rooks + 3 * knights + 3 * bishops + pawns
+                    matcountkey = 9 * queens + 5 * rooks + 3 * knights + 3 * bishops + pawns
 
-                if scorekey is not None:
-                    matstats[(resultkey, plieskey, matcountkey, scorekey)] += 1
+                    if scorekey is not None:
+                        matstats[(resultkey, plieskey, matcountkey, scorekey)] += 1
 
-                board = node.board()
-
-        pgnfilein.close()
+                    board = node.board()
+            pgnfilein.close()
+            
         return matstats
 
 if __name__ == "__main__":
@@ -110,11 +119,20 @@ if __name__ == "__main__":
     # map sharp_pos to all pgn files using an executor
     ana = PosAnalyser(args.matching_plies)
 
+    chunks_size = 100
+    pgnschunked = list(chunks(pgns, chunks_size))
+
     res = Counter()
-    with concurrent.futures.ProcessPoolExecutor() as e:
-        results = e.map(ana.ana_pos, pgns, chunksize=100)
-        for r in results:
-            res.update(r)
+    futures = []
+    
+    with tqdm(total=len(pgnschunked), smoothing=0, miniters=1) as pbar:
+        with concurrent.futures.ProcessPoolExecutor() as e:
+            for entry in pgnschunked:
+                futures.append(e.submit(ana.ana_pos, entry))
+
+            for future in concurrent.futures.as_completed(futures):
+                pbar.update(1)
+                res.update(future.result())
 
     # and print all the fens
     with open("scoreWLDstat.json", "w") as outfile:
