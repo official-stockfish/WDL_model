af4bce21da0a788ef8b210fb0bee448b2eeaa0ee
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2023-01-11 21:00:27 +0100

    Add secondary x-axis for pawn evaluation

--- scoreWLDana_moves_fit.py
+++ scoreWLDana_moves_fit.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
 from scipy.optimize import curve_fit
+import math
 
 parser = argparse.ArgumentParser()
 
@@ -111,6 +112,23 @@ def wdl(score, move, popt_as, popt_bs):
     return (w, d, l)
 
 
+def normalized_axis(ax):
+    ax2 = ax.twiny()
+    tickmin = math.ceil(ax.get_xlim()[0] / args.NormalizeToPawnValue) * 2
+    tickmax = math.floor(ax.get_xlim()[1] / args.NormalizeToPawnValue) * 2 + 1
+    new_tick_locations = np.array(
+        [x / 2 * args.NormalizeToPawnValue for x in range(tickmin, tickmax)]
+    )
+
+    def tick_function(X):
+        V = X / args.NormalizeToPawnValue
+        return [("%.0f" % z if z % 1 < 0.1 else "") for z in V]
+
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_xticks(new_tick_locations)
+    ax2.set_xticklabels(tick_function(new_tick_locations))
+
+
 #
 # convert to model, fit the winmodel a and b,
 # for a given value of the move counter
@@ -170,12 +188,14 @@ for m in range(3, 120, grouping):
             )
         axs[0, 0].plot(xdata, ymodel, "r-")
 
-        axs[0, 0].set_xlabel("Internal value units")
+        axs[0, 0].set_xlabel("Evaluation [lower: Internal Value units, upper: Pawns]")
         axs[0, 0].set_ylabel("outcome")
         axs[0, 0].legend(fontsize="small")
         axs[0, 0].set_title("Comparison of model and measured data at move 32")
         xmax = ((3 * args.NormalizeToPawnValue) // 100 + 1) * 100
         axs[0, 0].set_xlim([-xmax, xmax])
+        normalized_axis(axs[0, 0])
+
 
 #
 # now capture the functional behavior of a and b as a function of the move counter
@@ -229,77 +249,65 @@ axs[1, 0].set_ylim(bottom=0.0)
 #
 # now generate contour plots
 #
+contourlines = [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.97, 1.0]
+
 print("processing done, plotting 2D data")
 for i in [0, 1]:
     for j in [1, 2]:
         axs[i, j].yaxis.grid(True)
         axs[i, j].xaxis.grid(True)
-        axs[i, j].set_xlabel("internal Value units")
+        axs[i, j].set_xlabel("Evaluation [lower: Internal Value units, upper: Pawns]")
         axs[i, j].set_ylabel("move")
 
-# for wins, data
+# for wins
 xmin = -((1 * args.NormalizeToPawnValue) // 100 + 1) * 100
 xmax = ((3 * args.NormalizeToPawnValue) // 100 + 1) * 100
 grid_x, grid_y = np.mgrid[xmin:xmax:30j, 10:120:22j]
 points = np.array(list(zip(xs, ys)))
+
+# data
 zz = griddata(points, zs, (grid_x, grid_y), method="linear")
-cp = axs[0, 1].contourf(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0]
-)
+cp = axs[0, 1].contourf(grid_x, grid_y, zz, contourlines)
 fig.colorbar(cp, ax=axs[:, 2], shrink=0.618)
-CS = axs[0, 1].contour(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0], colors="black"
-)
+CS = axs[0, 1].contour(grid_x, grid_y, zz, contourlines, colors="black")
 axs[0, 1].clabel(CS, inline=1, colors="black")
 axs[0, 1].set_title("Data: Fraction of positions leading to a win")
+normalized_axis(axs[0, 1])
 
-# for wins, model
+# model
 for i in range(0, len(xs)):
     zs[i] = wdl(xs[i], ys[i], popt_as, popt_bs)[0] / 1000.0
-
-grid_x, grid_y = np.mgrid[xmin:xmax:30j, 10:120:22j]
-points = np.array(list(zip(xs, ys)))
 zz = griddata(points, zs, (grid_x, grid_y), method="linear")
-cp = axs[1, 1].contourf(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0]
-)
-CS = axs[1, 1].contour(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0], colors="black"
-)
+cp = axs[1, 1].contourf(grid_x, grid_y, zz, contourlines)
+CS = axs[1, 1].contour(grid_x, grid_y, zz, contourlines, colors="black")
 axs[1, 1].clabel(CS, inline=1, colors="black")
 axs[1, 1].set_title("Model: Fraction of positions leading to a win")
+normalized_axis(axs[1, 1])
 
 
-# for draws, data
+# for draws
 xmin = -((2 * args.NormalizeToPawnValue) // 100 + 1) * 100
 xmax = ((2 * args.NormalizeToPawnValue) // 100 + 1) * 100
 grid_x, grid_y = np.mgrid[xmin:xmax:30j, 10:120:22j]
 points = np.array(list(zip(xs, ys)))
+
+# data
 zz = griddata(points, zdraws, (grid_x, grid_y), method="linear")
-cp = axs[0, 2].contourf(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0]
-)
-CS = axs[0, 2].contour(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0], colors="black"
-)
+cp = axs[0, 2].contourf(grid_x, grid_y, zz, contourlines)
+CS = axs[0, 2].contour(grid_x, grid_y, zz, contourlines, colors="black")
 axs[0, 2].clabel(CS, inline=1, colors="black")
 axs[0, 2].set_title("Data: Fraction of positions leading to a draw")
+normalized_axis(axs[0, 2])
 
 # for wins, draws
 for i in range(0, len(xs)):
     zs[i] = wdl(xs[i], ys[i], popt_as, popt_bs)[1] / 1000.0
-
-grid_x, grid_y = np.mgrid[xmin:xmax:30j, 10:120:22j]
-points = np.array(list(zip(xs, ys)))
 zz = griddata(points, zs, (grid_x, grid_y), method="linear")
-cp = axs[1, 2].contourf(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0]
-)
-CS = axs[1, 2].contour(
-    grid_x, grid_y, zz, [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1.0], colors="black"
-)
+cp = axs[1, 2].contourf(grid_x, grid_y, zz, contourlines)
+CS = axs[1, 2].contour(grid_x, grid_y, zz, contourlines, colors="black")
 axs[1, 2].clabel(CS, inline=1, colors="black")
 axs[1, 2].set_title("Model: Fraction of positions leading to a draw")
+normalized_axis(axs[1, 2])
 
 
 fig.align_labels()
